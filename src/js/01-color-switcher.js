6591eef1278d1fc60c8fd2ce17b759e817f1e37f
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-16 21:56:52 +0200

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
 
+console.log(refs.btnStart);
+console.log(refs.btnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const handleStartClick = () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  //   refs.btnStart.setAttribute('disabled', true);
+  refs.btnStart.disabled = true;
+};
+const handleStopClick = () => {
+  clearInterval(timerId);
+  //   refs.btnStart.removeAttribute('disabled');
+  refs.btnStart.disabled = false;
+};
+
+refs.btnStart.addEventListener('click', handleStartClick);
+refs.btnStop.addEventListener('click', handleStopClick);
